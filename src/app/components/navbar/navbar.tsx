b8050aed367447d7db87c0d687de48bfc33df1ef
Tom Horne <tom@madamot.com>
2023-12-12 17:16:10 +0000

    added dropdown for hamburger menu

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -22,7 +22,7 @@ const NavBar: React.FC<NavBarProps> = ({title}) => {
       <nav className="flex justify-between items-center p-4 h-16 max-w-[1366px] mx-auto">
 
         <div>
-          <Link href="/" onClick={handleMenuIcon}>
+          <Link href="/">
               <h2>TH</h2>
               <h1 className="hidden md:flex text-white">{title}</h1>
             </Link>
@@ -49,7 +49,34 @@ const NavBar: React.FC<NavBarProps> = ({title}) => {
             ( <EllipsisVerticalIcon className="h-6 w-6" /> )
           }
         </div>
-        
+
+        <div className={menuIcon ?
+          "md:hidden absolute w-60 h-60 top-16 right-0 flex justify-center items-center bg-red-900 ease-in duration-300 rounded-b-lg"
+          :
+          "md:hidden absolute w-60 h-60 top-[-350%] right-0 flex justify-center items-center bg-red-900 ease-in duration-300 rounded-b-lg"
+        }>
+          
+          <div className="w-full">
+            <ul className="text-white mx-2">
+
+              
+                <Link href="/myprojects">
+                  <li onClick={handleMenuIcon} className="bg-purple-500 p-4 my-2 rounded-xl">
+                    Projects
+                  </li>
+                </Link> 
+
+              <li onClick={handleMenuIcon} className="bg-purple-500 p-4 my-2 rounded-xl">
+                <Link href="/about">About</Link>
+              </li>
+              
+              <li onClick={handleMenuIcon} className="bg-purple-500 p-4 my-2 rounded-xl">
+                <Link href="/university">University</Link>
+              </li>
+            </ul>
+          </div>
+
+        </div>
 
       </nav>
     </header>
