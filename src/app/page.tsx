b058998077ab83c2a0177513bbd5c11833db4703
Tom Horne <tom@madamot.com>
2023-12-21 01:10:35 +0000

    feat: fixed cardarea component. card area was in the map function styled card components to work with small screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
-import { getData } from "@/utils/getData"
-import { HOME_QUERY } from "@/graphql/queries"
+import { getData } from "@/utils/getData";
+import { HOME_QUERY } from "@/graphql/queries";
 import BlockBuilder from "@/components/blockbuilder";
 
-
 export default async function Home() {
+  const data = await getData({ query: HOME_QUERY });
 
-const data = await getData({ query: HOME_QUERY });
-
-return (
-  <main className="w-full mt-16 top-0 ">
-    {/* <div className="bg-blue-300 flex justify-center mx-auto max-w-[1366px] h-16">
+  return (
+    <main className="w-full mt-16 top-0 ">
+      {/* <div className="bg-blue-300 flex justify-center mx-auto max-w-[1366px] h-16">
       <h1>Welcome to my Portfolio!</h1>
     </div> */}
-    <BlockBuilder content={data?.home?.content}/>
-  </main>
-  )
+      <BlockBuilder content={data?.home?.content} />
+    </main>
+  );
 }
--- src/components/blockbuilder/blockbuilder.tsx
+++ src/components/blockbuilder/blockbuilder.tsx
@@ -5,24 +5,43 @@ import Card from "../card";
 import CardArea from "../cardarea";
 
 type BlockBuilderProps = {
-    content: any
-}
+  content: any;
+};
 
 const BlockBuilder: React.FC<BlockBuilderProps> = ({ content }) => {
-    return content?.map((component: any) => {
-        switch (component.__typename) {
-            case "TextRecord":
-                return <Text title={component.title} text={component.text} image={component.image}/>
-            case "BannerRecord":
-                return <Banner title={component.title} description={component.description} image={component.image}/>
-            case "CardRecord":
-                return <Card key={component.id} title={component.title} description={component.description} image={component.image}/>
-            case "CardareaRecord":
-                return <CardArea cards={component.cards} />
-            default:
-                return <div>No component found</div>
-        }
-    });
-}
+  return content?.map((component: any) => {
+    switch (component.__typename) {
+      case "TextRecord":
+        return (
+          <Text
+            title={component.title}
+            text={component.text}
+            image={component.image}
+          />
+        );
+      case "BannerRecord":
+        return (
+          <Banner
+            title={component.title}
+            description={component.description}
+            image={component.image}
+          />
+        );
+      case "CardRecord":
+        return (
+          <Card
+            key={component.id}
+            title={component.title}
+            description={component.description}
+            image={component.image}
+          />
+        );
+      case "CardareaRecord":
+        return <CardArea cards={component.cards} />;
+      default:
+        return <div>No component found</div>;
+    }
+  });
+};
 
-export default BlockBuilder
+export default BlockBuilder;
