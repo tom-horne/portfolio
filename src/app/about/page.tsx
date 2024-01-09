005edaba2263854c3f34e37b4eb77f2fc76fd2e1
Tom Horne <tom@madamot.com>
2024-01-09 15:55:16 +0000

    feat: created about page added about page query rearranged navbar links

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-// import { getData } from '@/utils/getData'
+import React from "react";
+import { getData } from "@/utils/getData";
+import { ABOUT_QUERY } from "@/graphql/queries";
+import BlockBuilder from "@/components/blockbuilder";
 
-const About = async () => {
-  // await getData()
+export default async function About() {
+  const data = await getData({ query: ABOUT_QUERY });
+
   return (
-    <h1 className="mt-16">All about me!</h1>
-  )
+    <main className="w-full mt-16 ">
+      <BlockBuilder content={data?.about?.content} />
+    </main>
+  );
 }
-
-export default About
--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -245,3 +245,60 @@ query University {
   }
 }
 `
+
+export const ABOUT_QUERY = `
+query About {
+  about {
+    content {
+      ... on TextRecord {
+        __typename
+        id
+        text
+      }
+      ... on HeaderRecord {
+        __typename
+        id
+        title
+        subtitle
+      }
+      ... on BannerRecord {
+        __typename
+        id
+        title
+        description
+        image {
+          responsiveImage(imgixParams: {fit: crop, w: "300", h: "300", auto: format}) {
+            sizes
+            src
+            width
+            height
+            alt
+            title
+            base64
+          }
+        }
+      }
+      ... on CardareaRecord {
+        __typename
+        id
+        cards {
+          title
+          description
+          link
+          image {
+            responsiveImage(imgixParams: {fit: crop, auto: format}) {
+              sizes
+              src
+              width
+              height
+              alt
+              title
+              base64
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
