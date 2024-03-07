715d1fb513f707372b74b0a4ff25cb8fa841f1ce
Tom Horne <tom@madamot.com>
2024-03-07 23:19:11 +0000

    test files for components

---
+++ src/components/cardarea/cardarea.test.tsx
@@ -0,0 +1,10 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+describe("< />", () => {
+  it("", () => {
+    render(<></>);
+
+    //   expect(screen.getByText("testTitle")).toBeInTheDocument();
+  });
+});
